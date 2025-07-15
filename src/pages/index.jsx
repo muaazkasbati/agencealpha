335168d62a8b90c2cd371d16730678306f1c1b76
ibad@digitalbee.studio <ibad@digitalbee.studio>
2025-07-15 17:08:36 +0500

    updated 15 july (1)

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,18 +7,95 @@ export default function Home() {
     <>
       <Header />
       <section className="py-16 mt-12">
-        <div className="container mx-auto">
+        <div className="">
           <div className="grid grid-cols-2 items-center gap-20">
             <div className="space-y-4 pl-10 pr-12">
               <h4 className="text-[16px] text-[#00cef7] uppercase font-semibold">Agence génération leads qualifiés</h4>
               <h1 className="text-[40px] text-[#0088cc] font-bold">Développer votre business n’a jamais été aussi facile</h1>
               <p className="text-[16px] text-[#666] leading-8">Experts en <b>génération de leads</b>, nous vous apportons des prospects <b>qualifiés</b> et répondant à vos critères de qualification. Nous activons pour cela un ensemble de leviers de marketing digital qui nous permettront d’identifier les intentions et les recherches de vos futurs clients.</p>
+              <button className="bg-[#f26d00] hover:bg-[#f26d00]/80 uppercase transition-all duration-300 cursor-pointer rounded-3xl py-4 px-8 flex items-center text-[12px] font-semibold text-white gap-1">
+                Demander un devis
+                <span className="bg-white h-[12px] w-[12px] rounded-full text-[#f26d00]">
+                  <svg xmlns="http://www.w3.org/2000/svg" height={12} width={12} viewBox="0 0 48 48" fill="currentColor" class="size-6">
+                    <path fill-rule="evenodd" d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clip-rule="evenodd" />
+                  </svg>
+                </span>
+              </button>
             </div>
-            <div className="bg-[#0088cc] relative rounded-l-[60px] text-white pt-20 bg-[url(data:image/svg+xml;base64,PHN2ZyAgZmlsbD0icmdiYSgyNTUsMjU1LDI1NSwwLjA3KSIgaGVpZ2h0PSIxNjBweCIgd2lkdGg9IjgwcHgiIHZpZXdCb3g9IjAgMCA4MCAxNjAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTQxLjEyLDQwLDUzLDE2LjI4LDYwLjYyLDFIODBWMEg1OC44OEw0MCwzNy43NiwyMS4xMiwwSDBWMUgxOS4zOEwyNywxNi4yOCwzOC44OCw0MCwxOS4zOCw3OUgwdjJIMTkuMzhsMTkuNSwzOS0xOS41LDM5SDB2MUgyMS4xMkw0MCwxMjIuMjQsNTguODgsMTYwSDgwdi0xSDYwLjYybC0xOS41LTM5LDE5LjUtMzlIODBWNzlINjAuNjJaTTQwLDExNy43NiwyMS4xMiw4MCw0MCw0Mi4yNCw1OC44OCw4MFoiLz48L3N2Zz4=)]">
-            <img src="https://agencealpha.fr/wp-content/uploads/2022/06/agence-alpha-generation-de-leads-men-1.png" alt="" className="w-[80%] relative z-10 -ml-30" />
-            <img src="https://agencealpha.fr/wp-content/uploads/2022/05/screen-5.png" alt="" className="w-[580px] z-0 absolute left-30 top-1/3 block mx-auto" />
+            <div className="bg-[#0088cc] relative rounded-l-[60px] text-white pt-24 bg-[url(data:image/svg+xml;base64,PHN2ZyAgZmlsbD0icmdiYSgyNTUsMjU1LDI1NSwwLjA3KSIgaGVpZ2h0PSIxNjBweCIgd2lkdGg9IjgwcHgiIHZpZXdCb3g9IjAgMCA4MCAxNjAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTQxLjEyLDQwLDUzLDE2LjI4LDYwLjYyLDFIODBWMEg1OC44OEw0MCwzNy43NiwyMS4xMiwwSDBWMUgxOS4zOEwyNywxNi4yOCwzOC44OCw0MCwxOS4zOCw3OUgwdjJIMTkuMzhsMTkuNSwzOS0xOS41LDM5SDB2MUgyMS4xMkw0MCwxMjIuMjQsNTguODgsMTYwSDgwdi0xSDYwLjYybC0xOS41LTM5LDE5LjUtMzlIODBWNzlINjAuNjJaTTQwLDExNy43NiwyMS4xMiw4MCw0MCw0Mi4yNCw1OC44OCw4MFoiLz48L3N2Zz4=)]">
+              <img src="https://agencealpha.fr/wp-content/uploads/2022/06/agence-alpha-generation-de-leads-men-1.png" alt="" className="h-full relative z-10 -ml-30" />
+              <img src="https://agencealpha.fr/wp-content/uploads/2022/05/screen-5.png" alt="" className="w-[580px] z-0 absolute left-30 top-1/3 block mx-auto" />
             </div>
 
+          </div>
+        </div>
+      </section>
+      <section className="bg-[url(https://agencealpha.fr/wp-content/uploads/2022/03/app-19.png)] mb-3 shadow-xl bg-size-[50%] bg-no-repeat bg-center relative py-10 before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-white/80">
+        <div className="container mx-auto relative z-10 pt-20 text-center max-w-6xl">
+          <div className="mx-auto block w-14 h-4 bg-[#1ca5ff] mb-12"></div>
+          <h4 className="text-[16px] text-[#00cef7] uppercase font-semibold mb-12">Génération de leads</h4>
+          <h2 className="text-[40px] text-[#0088cc] font-bold mb-2">Une expertise métier à 360°</h2>
+          <p className="text-[16px] text-[#666] leading-8 mb-6">Nous générons pour vous <b>des leads qualifiés</b> sur votre secteur pour <b>faire décoller votre business</b></p>
+          <div className="grid grid-cols-5 mb-12">
+            <div className="text-center space-y-3">
+              <svg class="w-[68px] h-[68px] text-[#0c71c3] block mx-auto" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                <path fill-rule="evenodd" d="M12.037 21.998a10.313 10.313 0 0 1-7.168-3.049 9.888 9.888 0 0 1-2.868-7.118 9.947 9.947 0 0 1 3.064-6.949A10.37 10.37 0 0 1 12.212 2h.176a9.935 9.935 0 0 1 6.614 2.564L16.457 6.88a6.187 6.187 0 0 0-4.131-1.566 6.9 6.9 0 0 0-4.794 1.913 6.618 6.618 0 0 0-2.045 4.657 6.608 6.608 0 0 0 1.882 4.723 6.891 6.891 0 0 0 4.725 2.07h.143c1.41.072 2.8-.354 3.917-1.2a5.77 5.77 0 0 0 2.172-3.41l.043-.117H12.22v-3.41h9.678c.075.617.109 1.238.1 1.859-.099 5.741-4.017 9.6-9.746 9.6l-.215-.002Z" clip-rule="evenodd" />
+              </svg>
+              <h4 className="text-[18px]"><a href="#" className="text-black">Google Ads</a></h4>
+            </div>
+            <div className="text-center space-y-3">
+              <svg class="w-[68px] h-[68px] text-[#0c71c3] block mx-auto" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                <path fill-rule="evenodd" d="M12.037 21.998a10.313 10.313 0 0 1-7.168-3.049 9.888 9.888 0 0 1-2.868-7.118 9.947 9.947 0 0 1 3.064-6.949A10.37 10.37 0 0 1 12.212 2h.176a9.935 9.935 0 0 1 6.614 2.564L16.457 6.88a6.187 6.187 0 0 0-4.131-1.566 6.9 6.9 0 0 0-4.794 1.913 6.618 6.618 0 0 0-2.045 4.657 6.608 6.608 0 0 0 1.882 4.723 6.891 6.891 0 0 0 4.725 2.07h.143c1.41.072 2.8-.354 3.917-1.2a5.77 5.77 0 0 0 2.172-3.41l.043-.117H12.22v-3.41h9.678c.075.617.109 1.238.1 1.859-.099 5.741-4.017 9.6-9.746 9.6l-.215-.002Z" clip-rule="evenodd" />
+              </svg>
+              <h4 className="text-[18px]"><a href="#" className="text-black">Facebook Ads</a></h4>
+            </div>
+            <div className="text-center space-y-3">
+              <svg class="w-[68px] h-[68px] text-[#0c71c3] block mx-auto" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                <path fill-rule="evenodd" d="M12.037 21.998a10.313 10.313 0 0 1-7.168-3.049 9.888 9.888 0 0 1-2.868-7.118 9.947 9.947 0 0 1 3.064-6.949A10.37 10.37 0 0 1 12.212 2h.176a9.935 9.935 0 0 1 6.614 2.564L16.457 6.88a6.187 6.187 0 0 0-4.131-1.566 6.9 6.9 0 0 0-4.794 1.913 6.618 6.618 0 0 0-2.045 4.657 6.608 6.608 0 0 0 1.882 4.723 6.891 6.891 0 0 0 4.725 2.07h.143c1.41.072 2.8-.354 3.917-1.2a5.77 5.77 0 0 0 2.172-3.41l.043-.117H12.22v-3.41h9.678c.075.617.109 1.238.1 1.859-.099 5.741-4.017 9.6-9.746 9.6l-.215-.002Z" clip-rule="evenodd" />
+              </svg>
+              <h4 className="text-[18px]"><a href="#" className="text-black">Emailing</a></h4>
+            </div>
+            <div className="text-center space-y-3">
+              <svg class="w-[68px] h-[68px] text-[#0c71c3] block mx-auto" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                <path fill-rule="evenodd" d="M12.037 21.998a10.313 10.313 0 0 1-7.168-3.049 9.888 9.888 0 0 1-2.868-7.118 9.947 9.947 0 0 1 3.064-6.949A10.37 10.37 0 0 1 12.212 2h.176a9.935 9.935 0 0 1 6.614 2.564L16.457 6.88a6.187 6.187 0 0 0-4.131-1.566 6.9 6.9 0 0 0-4.794 1.913 6.618 6.618 0 0 0-2.045 4.657 6.608 6.608 0 0 0 1.882 4.723 6.891 6.891 0 0 0 4.725 2.07h.143c1.41.072 2.8-.354 3.917-1.2a5.77 5.77 0 0 0 2.172-3.41l.043-.117H12.22v-3.41h9.678c.075.617.109 1.238.1 1.859-.099 5.741-4.017 9.6-9.746 9.6l-.215-.002Z" clip-rule="evenodd" />
+              </svg>
+              <h4 className="text-[18px]"><a href="#" className="text-black">Linkedin Ads</a></h4>
+            </div>
+            <div className="text-center space-y-3">
+              <svg class="w-[68px] h-[68px] text-[#0c71c3] block mx-auto" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                <path fill-rule="evenodd" d="M12.037 21.998a10.313 10.313 0 0 1-7.168-3.049 9.888 9.888 0 0 1-2.868-7.118 9.947 9.947 0 0 1 3.064-6.949A10.37 10.37 0 0 1 12.212 2h.176a9.935 9.935 0 0 1 6.614 2.564L16.457 6.88a6.187 6.187 0 0 0-4.131-1.566 6.9 6.9 0 0 0-4.794 1.913 6.618 6.618 0 0 0-2.045 4.657 6.608 6.608 0 0 0 1.882 4.723 6.891 6.891 0 0 0 4.725 2.07h.143c1.41.072 2.8-.354 3.917-1.2a5.77 5.77 0 0 0 2.172-3.41l.043-.117H12.22v-3.41h9.678c.075.617.109 1.238.1 1.859-.099 5.741-4.017 9.6-9.746 9.6l-.215-.002Z" clip-rule="evenodd" />
+              </svg>
+              <h4 className="text-[18px]"><a href="#" className="text-black">SMS</a></h4>
+            </div>
+          </div>
+          <h2 className="text-[40px] text-[#0088cc] font-bold mb-4">Et bien plus encore…</h2>
+          <div className="flex justify-center">
+            <button className="bg-[#f26d00] hover:bg-[#f26d00]/80 uppercase transition-all duration-300 cursor-pointer rounded-3xl py-4 px-8 flex items-center text-[12px] font-semibold text-white gap-1">
+              nos leads
+              <span className="bg-white h-[12px] w-[12px] rounded-full text-[#f26d00]">
+                <svg xmlns="http://www.w3.org/2000/svg" height={12} width={12} viewBox="0 0 48 48" fill="currentColor" class="size-6">
+                  <path fill-rule="evenodd" d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clip-rule="evenodd" />
+                </svg>
+              </span>
+            </button>
+          </div>
+        </div>
+      </section>
+      <section className="py-16">
+        <div className="container mx-auto relative z-10 pt-20 text-center max-w-8xl">
+          <h4 className="text-[16px] text-[#00cef7] uppercase font-semibold mb-6">Nos leads</h4>
+          <h2 className="text-[40px] text-[#0088cc] font-bold mb-2">Une multitude de leads pour répondre à vos attentes</h2>
+          <p className="text-[16px] text-[#666] leading-8 mb-6">Plus de <b>100 000 leads déjà vendus</b> sur plus de <b>30 secteurs différents</b>.<br />
+            Découvrez quelques-uns de nos domaines d'expertises</p>
+          <div className="flex justify-center">
+            <button className="bg-[#f26d00] hover:bg-[#f26d00]/80 uppercase transition-all duration-300 cursor-pointer rounded-3xl py-4 px-8 flex items-center text-[12px] font-semibold text-white gap-1">
+              Demander un devis
+              <span className="bg-white h-[12px] w-[12px] rounded-full text-[#f26d00]">
+                <svg xmlns="http://www.w3.org/2000/svg" height={12} width={12} viewBox="0 0 48 48" fill="currentColor" class="size-6">
+                  <path fill-rule="evenodd" d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clip-rule="evenodd" />
+                </svg>
+              </span>
+            </button>
           </div>
         </div>
       </section>
